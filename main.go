ebb6117b9df1a8d42e0560d3bf1e4652457cc092
yanuar-bridge-note <muhammad_yanuar_fadhilah@bridgenote.id>
2023-03-26 15:24:42 +0700

    basic lear loop

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets uint = 50
 	remainingTickets := conferenceTickets
-	var firstName string
-	var lastName string
-	var emailAddress string
-	var userTickets uint
+
 	var bookings []string
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T", conferenceTickets, remainingTickets, conferenceName)
@@ -18,28 +18,41 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	// ask user for their name
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+	for remainingTickets != 0 {
+		var firstName string
+		var lastName string
+		var emailAddress string
+		var userTickets uint
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&emailAddress)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&emailAddress)
 
-	if userTickets > remainingTickets {
-		fmt.Println("Sorry tickets that you bought is over capacity, please enter below or same as remaining tickets. Thank you")
-	} else {
-		remainingTickets = remainingTickets - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
 
-		fmt.Printf("Thank you %v %v for booking %v tickets.\nYou Will recieve confirmation email at %v\n", firstName, lastName, userTickets, emailAddress)
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		if userTickets > remainingTickets {
+			fmt.Println("Sorry tickets that you bought is over capacity, please enter below or same as remaining tickets. Thank you")
+		} else {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets.\nYou will recieve confirmation email at %v\n", firstName, lastName, userTickets, emailAddress)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				var firstName = names[0]
+				firstNames = append(firstNames, firstName)
+			}
+			fmt.Printf("These first name of bookings for %v are %v\n", conferenceName, firstNames)
+		}
 	}
 
-	fmt.Printf("These are list of %v booking %v ticekts\n", conferenceName, bookings)
 }
