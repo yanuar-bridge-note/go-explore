8975443d0431f3cf74690e2ceeaa81c48deee31a
yanuar-bridge-note <muhammad_yanuar_fadhilah@bridgenote.id>
2023-03-26 14:41:43 +0700

    Getting User Input

--- main.go
+++ main.go
@@ -13,10 +13,24 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	userName := "Yanuar"
-	userTickets := 2
+	var firstName string
+	var lastName string
+	var emailAddress string
+	var userTickets int
 
 	// ask user for their name
-	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&emailAddress)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets.\nYou Will recieve confirmation email at %v\n", firstName, lastName, userTickets, emailAddress)
 
 }
